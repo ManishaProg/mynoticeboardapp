4e88669e9873589fc6a30b604d08a309a021e984
Mirana <vanshmanisha1995@gmail.com>
2018-06-15 18:14:55 +0530

    adding server files

---
+++ serverfiles/config.php
@@ -0,0 +1,7 @@
+<?php 
+ define('DB_USERNAME','root');
+ define('DB_PASSWORD','');
+ define('DB_NAME','noticeappdb');
+ define('DB_HOST','localhost');
+ //defined a new constant for firebase api key
+ define('FIREBASE_API_KEY', 'AAAAvTvwR-g:APA91bFcgXjH9jsv1tLpZgwSTRXTfixD7AhLIof0UMSzJcj3aqFHsCdtVSQd_n1hXStmUesl75Syv46jE_fk74qrscGp8J3fcjpJAEseLQImdge_nOoksUy-e1WdLZnKsH5EzfvMZPM1');
